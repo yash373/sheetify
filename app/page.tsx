be5d305c25123a63360462e4dbb0c299e973ba46
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2026-09-20 12:57:04 +0530

    fix: render homepage client boundary directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { HomePage } from "@/components/home-page";
 
-const ClientHomePage = dynamic(() => Promise.resolve(HomePage), { ssr: false });
-
 export default function Home() {
-  return <ClientHomePage />;
+  return <HomePage />;
 }
